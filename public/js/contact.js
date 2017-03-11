47684f701eb69484e7f0c9dc720938e568b4a0ac
mtuduri <tuduri.matias@gmail.com>
2017-03-11 16:43:28 -0300

    arreglo envio mail contacto

--- public/js/contact.js
+++ public/js/contact.js
@@ -4,16 +4,15 @@ $(function () {
 
     $('#contact-form').on('submit', function (e) {
         if (!e.isDefaultPrevented()) {
-            var url = "contact.php";
+            var url = " http://localhost:65320/api/email";
 
             $.ajax({
                 type: "POST",
                 url: url,
-                data: $(this).serialize(),
-                success: function (data)
-                {
-                    var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
+                data: { name: $("#form_name").val(), email: $("#form_email").val(), message: $("#form_message").val() },
+                success: function (message, status, xhr) {
+                    var messageAlert = 'alert-' + (xhr && xhr.status == 200 ? 'success' : 'error');
+                    var messageText = message;
 
                     var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
                     if (messageAlert && messageText) {
