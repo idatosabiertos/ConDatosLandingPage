a1c82f5b6e63056c87b3672f8f414371ce48edbc
Matias Tuduri <Matias.Tuduri@VERIFONE.com>
2017-03-15 12:26:33 -0300

    Added gulp task in order to replace minified files Added Readme file Improved package json

---
+++ README.md
@@ -0,0 +1,20 @@
+Condatos Landing Page
+==============
+
+
+Prerequisites
+--------------
+- Node js
+- npm
+
+
+Installation
+--------------
+- Just run in your command line **npm start** under **/public** folder
+- Then navigate to **http://localhost:8000**
+
+
+Production
+--------------
+- Just run in your command line **gulp** under **/public** folder 
+- The result files will be under **/dist** folder
--- public/GulpFile.js
+++ public/GulpFile.js
@@ -3,20 +3,21 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var strip = require('gulp-strip-comments');
 var minify = require('gulp-minify-css');
+var htmlreplace = require('gulp-html-replace');
 
-gulp.task('js', function(){
-   gulp.src(['js/*.js','!js/jquery-3.1.1.min.js'])
-   .pipe(concat('script.js'))
-   .pipe(strip())
-   .pipe(uglify())
-   .pipe(gulp.dest('dist/js/'));
+gulp.task('js', function() {
+    gulp.src(['js/*.js', '!js/jquery-3.1.1.min.js'])
+        .pipe(concat('script.js'))
+        .pipe(strip())
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/'));
 });
 
-gulp.task('css', function(){
-   gulp.src('css/*.css')
-   .pipe(concat('style.css'))
-   .pipe(minify({keepSpecialComments : 0}))
-   .pipe(gulp.dest('dist/css/'));
+gulp.task('css', function() {
+    gulp.src('css/*.css')
+        .pipe(concat('style.css'))
+        .pipe(minify({ keepSpecialComments: 0 }))
+        .pipe(gulp.dest('dist/css/'));
 });
 
 gulp.task('copy', function() {
@@ -24,7 +25,7 @@ gulp.task('copy', function() {
         .pipe(gulp.dest('dist/js/'))
     gulp.src(['index.html'])
         .pipe(gulp.dest('dist/'))
-    gulp.src(['css/overlays/02.png'])
+    gulp.src(['css/overlays/*'])
         .pipe(gulp.dest('dist/css/overlays/'))
     gulp.src('img/**/*')
         .pipe(gulp.dest('dist/img/'))
@@ -32,5 +33,15 @@ gulp.task('copy', function() {
         .pipe(gulp.dest('dist/fonts/'))
 });
 
-gulp.task('default',['js','css','copy'],function(){
+
+gulp.task('replaceIndexFiles', function() {
+    gulp.src('index.html')
+        .pipe(htmlreplace({
+            'css': 'css/style.css',
+            'js': 'js/script.js'
+        }))
+        .pipe(gulp.dest('dist/'));
 });
+
+
+gulp.task('default', ['js', 'css', 'copy', 'replaceIndexFiles'], function() {});
